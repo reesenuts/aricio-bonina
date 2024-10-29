e9085d88b364c0b3d3ffd905819001b46e929161
0.8lili <130875766+reesenuts@users.noreply.github.com>
2024-10-29 16:40:06 +0800

    initial frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section >
+    <div class="bg-black text-white p-4">
+        <h1 class="text-4xl">Aricio D. Bonina</h1>
+        <h2 class="text-lg">Real Estate Leasing</h2>
+    </div>
+</section>
