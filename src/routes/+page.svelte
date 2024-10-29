70a074a7abd3449db138e1dba8ce30e512d4e4e7
0.8lili <130875766+reesenuts@users.noreply.github.com>
2024-10-29 16:45:45 +0800

    second commit for frontend initialization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <section >
-    <div class="bg-black text-white p-4">
-        <h1 class="text-4xl">Aricio D. Bonina</h1>
+    <div class="bg-black text-white p-5">
+        <h1 class="text-5xl font-bold">Aricio D. Bonina</h1>
         <h2 class="text-lg">Real Estate Leasing</h2>
     </div>
+
+    
 </section>
